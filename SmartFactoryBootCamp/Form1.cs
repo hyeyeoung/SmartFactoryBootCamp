fb4b77b25026895628662b68f6a763eaa517ad70
hyeyeoung <rlagpdud0524@gmail.com>
2024-12-19 12:26:39 +0900

    switch

--- SmartFactoryBootCamp/Form1.cs
+++ SmartFactoryBootCamp/Form1.cs
@@ -15,60 +15,86 @@ namespace SmartFactoryBootCamp
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        int theCoinThrowAndMatch(string inputCoin)
+        enum Days
         {
-            Random random = new Random();
-            int computerMake = random.Next();
-            string resultComputer = (computerMake % 2 == 1).ToString();
-            if(inputCoin == "True" || inputCoin == "False")
+            Monday, 
+            Tuesday,
+            Wednesday, 
+            Thursday, 
+            Friday, 
+            Saturday,
+            Sunday,
+            error
+        }
+        Days theChoiceWeekday(string message){
+            switch (message)
             {
-                if (inputCoin == resultComputer)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 2;
-                }
-            }
-            else
-            {
-                return -1;
+                case "월요일":
+                    return Days.Monday;
+                case "화요일":
+                    return Days.Tuesday;
+                case "수요일":
+                    return Days.Wednesday;
+                case "목요일":
+                    return Days.Thursday;
+                case "금요일":
+                    return Days.Friday;
+                case "토요일":
+                    return Days.Saturday;
+                case "일요일":
+                    return Days.Sunday;
+                default:
+                    return Days.error;
             }
         }
         private void botton_input_Click(object sender, EventArgs e)
         {
-            textBox_result.Text = "입력하신 값은 " + textBox_input.Text + "입니다\r\n";
-            int theCoinMatchResult = theCoinThrowAndMatch(textBox_input.Text);
-            
-            switch (theCoinMatchResult){
-                case 1:
-                    textBox_result.Text += "동전 던지기 결과는 \r\n승리입니다.";
-                    break;
-                case 2:
-                    textBox_result.Text += "동전 던지기 결과는 \r\n패배입니다.";
-                    break;
-                case -1:
-                    textBox_result.Text += "승부가 불가합니다. \r\n입력을 다시 해주세요.";
-                    break;
-                default:
-                    break;
+            if (textBox_input.Text.Length > 0)
+            {
+                Days input = theChoiceWeekday(textBox_input.Text);
+                switch (input)
+                {
+
+                    case Days.Monday:
+                        textBox_result.Text = "심근경색 위험! 출근보다 침대가 내 건강을 더 생각한다.";
+                        break;
+                    case Days.Tuesday:
+                        textBox_result.Text = "월요일을 이겨냈으니 이제는... 또 다른 월요일이 시작된 느낌.";
+                        break;
+                    case Days.Wednesday:
+                        textBox_result.Text = "주말의 절반이 더 가까워졌다고? 그게 나한테 무슨 의미지?";
+                        break;
+                    case Days.Thursday:
+                        textBox_result.Text = "금요일 전날이라는 이유만으로 사랑받는 유일한 요일.";
+                        break;
+                    case Days.Friday:
+                        textBox_result.Text = "드디어 금요일! 하지만 월급은 아직 멀었지...";
+                        break;
+                    case Days.Saturday:
+                        textBox_result.Text = "오늘은 알람이 필요 없다. 내 몸이 나를 배신하기 전까지 자겠다.";
+                        break;
+                    case Days.Sunday:
+                        textBox_result.Text = "내일이 월요일이라는 사실을 제외하면 완벽한 하루.";
+                        break;
+                    default:
+                        textBox_result.Text = "요일이 아닙니다.";
+                        break;
+                }
             }
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            textBox_input.Text = true.ToString();
         }
         private void radioButton_false_CheckedChanged(object sender, EventArgs e)
         {
-            textBox_input.Text = false.ToString();
         }
     }
 }
