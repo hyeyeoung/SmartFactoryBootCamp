421a4e3fa24a6fbb6b50c271aae80153e2ac042f
hyeyeoung <rlagpdud0524@gmail.com>
2024-12-19 12:32:11 +0900

    change

--- SmartFactoryBootCamp/Form1.cs
+++ SmartFactoryBootCamp/Form1.cs
@@ -33,54 +33,54 @@ namespace SmartFactoryBootCamp
             Sunday,
             error
         }
-        Days theChoiceWeekday(string message){
+        int theChoiceWeekday(string message){
             switch (message)
             {
                 case "월요일":
-                    return Days.Monday;
+                    return 0;
                 case "화요일":
-                    return Days.Tuesday;
+                    return 1;
                 case "수요일":
-                    return Days.Wednesday;
+                    return 2;
                 case "목요일":
-                    return Days.Thursday;
+                    return 3;
                 case "금요일":
-                    return Days.Friday;
+                    return 4;
                 case "토요일":
-                    return Days.Saturday;
+                    return 5;
                 case "일요일":
-                    return Days.Sunday;
+                    return 6;
                 default:
-                    return Days.error;
+                    return -1;
             }
         }
         private void botton_input_Click(object sender, EventArgs e)
         {
             if (textBox_input.Text.Length > 0)
             {
-                Days input = theChoiceWeekday(textBox_input.Text);
+                int input = theChoiceWeekday(textBox_input.Text);
                 switch (input)
                 {
 
-                    case Days.Monday:
+                    case (int)Days.Monday:
                         textBox_result.Text = "심근경색 위험! 출근보다 침대가 내 건강을 더 생각한다.";
                         break;
-                    case Days.Tuesday:
+                    case (int)Days.Tuesday:
                         textBox_result.Text = "월요일을 이겨냈으니 이제는... 또 다른 월요일이 시작된 느낌.";
                         break;
-                    case Days.Wednesday:
+                    case (int)Days.Wednesday:
                         textBox_result.Text = "주말의 절반이 더 가까워졌다고? 그게 나한테 무슨 의미지?";
                         break;
-                    case Days.Thursday:
+                    case (int)Days.Thursday:
                         textBox_result.Text = "금요일 전날이라는 이유만으로 사랑받는 유일한 요일.";
                         break;
-                    case Days.Friday:
+                    case (int)Days.Friday:
                         textBox_result.Text = "드디어 금요일! 하지만 월급은 아직 멀었지...";
                         break;
-                    case Days.Saturday:
+                    case (int)Days.Saturday:
                         textBox_result.Text = "오늘은 알람이 필요 없다. 내 몸이 나를 배신하기 전까지 자겠다.";
                         break;
-                    case Days.Sunday:
+                    case (int)Days.Sunday:
                         textBox_result.Text = "내일이 월요일이라는 사실을 제외하면 완벽한 하루.";
                         break;
                     default:
