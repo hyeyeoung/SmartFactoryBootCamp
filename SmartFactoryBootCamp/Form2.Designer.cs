34d40268f4d9aad47804c0411499f29b7b3e96ca
hyeyeoung <rlagpdud0524@gmail.com>
2024-12-20 12:50:03 +0900

    RSP

---
+++ SmartFactoryBootCamp/Form2.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SmartFactoryBootCamp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scissors = new System.Windows.Forms.Button();
+            this.rock = new System.Windows.Forms.Button();
+            this.papper = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // scissors
+            // 
+            this.scissors.Location = new System.Drawing.Point(12, 12);
+            this.scissors.Name = "scissors";
+            this.scissors.Size = new System.Drawing.Size(75, 23);
+            this.scissors.TabIndex = 0;
+            this.scissors.Text = "가위";
+            this.scissors.UseVisualStyleBackColor = true;
+            this.scissors.Click += new System.EventHandler(this.scissors_Click);
+            // 
+            // rock
+            // 
+            this.rock.Location = new System.Drawing.Point(131, 12);
+            this.rock.Name = "rock";
+            this.rock.Size = new System.Drawing.Size(75, 23);
+            this.rock.TabIndex = 1;
+            this.rock.Text = "바위";
+            this.rock.UseVisualStyleBackColor = true;
+            this.rock.Click += new System.EventHandler(this.rock_Click);
+            // 
+            // papper
+            // 
+            this.papper.Location = new System.Drawing.Point(250, 12);
+            this.papper.Name = "papper";
+            this.papper.Size = new System.Drawing.Size(75, 23);
+            this.papper.TabIndex = 2;
+            this.papper.Text = "보";
+            this.papper.UseVisualStyleBackColor = true;
+            this.papper.Click += new System.EventHandler(this.papper_Click);
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(12, 59);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(327, 115);
+            this.resultBox.TabIndex = 3;
+            this.resultBox.TextChanged += new System.EventHandler(this.resultBox_TextChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 200);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.papper);
+            this.Controls.Add(this.rock);
+            this.Controls.Add(this.scissors);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button scissors;
+        private System.Windows.Forms.Button rock;
+        private System.Windows.Forms.Button papper;
+        private System.Windows.Forms.TextBox resultBox;
+    }
+}
---
+++ SmartFactoryBootCamp/Form2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartFactoryBootCamp
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+        }
+
+        int computerRSP()
+        {
+            Random r = new Random();
+            int x = r.Next(0, 3);
+            return x;
+        }
+        int use_win = 0;
+        int computer_win = 0;
+        string[] match = { "가위", "바위", "보" };
+
+        private void Match(int idx, int computer)
+        {
+            if (idx == 0 && computer == 2)
+            {
+                use_win++;
+            }
+            else if (idx == 1 && computer == 0)
+            {
+                use_win++;
+            }
+            else if (idx == 2 && computer == 1)
+            {
+                use_win++;
+            }
+            else if (idx == 2 && computer == 0)
+            {
+                computer_win++;
+            }
+            else if (idx == 0 && computer == 1)
+            {
+                computer_win++;
+            }
+            else if (idx == 1 && computer == 2)
+            {
+                computer_win++;
+            }
+            resultBox.Text += "사용자: " + match[idx] + " 컴퓨터: " + match[computer] + " | 점수 사용자: " + use_win.ToString() + " 컴퓨터: " + computer_win.ToString()+ "\r\n";
+            
+            if (use_win == 3 || computer_win == 3)
+            {
+                if(use_win == 3 && computer != 3)
+                {
+                    resultBox.Text += "사용자 승리\r\n";
+                }
+                else if(use_win != 3 || computer_win == 3)
+                {
+                    resultBox.Text += "컴퓨터 승리\r\n";
+                }
+                else
+                {
+                    resultBox.Text += "무승부\r\n";
+                }
+            }
+        }
+        private void scissors_Click(object sender, EventArgs e)
+        {
+            int idx = 0;
+            int computer = computerRSP();
+            Match(idx, computer);
+        }
+        private void rock_Click(object sender, EventArgs e)
+        {
+            int idx = 1;
+            int computer = computerRSP();
+            Match(idx, computer);
+        }
+        private void papper_Click(object sender, EventArgs e)
+        {
+            int idx = 2;
+            int computer = computerRSP();
+            Match(idx, computer);
+        }
+
+        private void resultBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
