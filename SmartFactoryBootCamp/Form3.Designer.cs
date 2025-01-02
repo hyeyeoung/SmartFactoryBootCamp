1a0c9288c00d7b98353bdda3a152b1c2f19db168
hyeyeoung <rlagpdud0524@gmail.com>
2025-01-02 11:34:21 +0900

    ref, out 사용

--- SmartFactoryBootCamp/Form3.Designer.cs
+++ SmartFactoryBootCamp/Form3.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 256);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(375, 301);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- SmartFactoryBootCamp/Form3.cs
+++ SmartFactoryBootCamp/Form3.cs
@@ -14,27 +14,67 @@ namespace SmartFactoryBootCamp
 {
     public partial class Form3 : Form
     {
+        public void fillNum(ref int [] arr)
+        {
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                arr[i] = i + 1;
+            }
+        }
+
+        public void fillNum(out int[] arr, int n)
+        {
+            arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = i + 1;
+            }
+        }
         public Form3()
         {
             InitializeComponent();
+            //Character.Character new1 = new Character.NPC("NPC","마을 주민", "주민", 30, 15);
+            //Character.Character new2 = new Character.Monster("Monster", "괴물", "slime", 500, 20);
+            //Character.Character new3 = new Character.Player("Player", "용사", "fighter", 100, 80);
 
-            Character.Character new1 = new Character.NPC("NPC","마을 주민", "주민", 30, 15);
-            Character.Character new2 = new Character.Monster("Monster", "괴물", "slime", 500, 20);
-            Character.Character new3 = new Character.Player("Player", "용사", "fighter", 100, 80);
+            //new1.Talk();
+            //new2.Talk();
+            //new3.Talk();
 
-            new1.Talk();
-            new2.Talk();
-            new3.Talk();
+            //fight f1 = new fight(((Character.NPC)new1).NPCrespone(), ((Character.Monster)new2).Monsterrespone());
+            //fight f2 = new fight(((Character.Monster)new2).Monsterrespone(), ((Character.Player)new3).playrespone());
 
-            fight f1 = new fight(((Character.NPC)new1).NPCrespone(), ((Character.Monster)new2).Monsterrespone());
-            fight f2 = new fight(((Character.Monster)new2).Monsterrespone(), ((Character.Player)new3).playrespone());
+            //MessageBox.Show("첫 번째 라운드: " + f1.sendResult().ToString());
+            //MessageBox.Show("두 번째 라운드: " + f2.sendResult().ToString());
 
-            MessageBox.Show("첫 번째 라운드: " + f1.sendResult().ToString());
-            MessageBox.Show("두 번째 라운드: " + f2.sendResult().ToString());
+            //new1.levelUP();
+            //new2.levelUP(20);
+            //new3.levelUP(20, 30);
 
-            new1.levelUP();
-            new2.levelUP(20);
-            new3.levelUP(20, 30);
+            // ref 사용
+            int[] arr1 = new int[10];
+            fillNum(ref arr1);
+            textBox1.Text = "";
+            foreach (int i in arr1)
+            {
+                textBox1.Text += i;
+            }
+            textBox1.Text += "\r\n";
+            
+            // out 사용
+            int[] arr2;
+            fillNum(out arr2, 10);
+            foreach (int i in arr2)
+            {
+                textBox1.Text += i;
+            }
+            textBox1.Text += "\r\n";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // 1번 ref
+            
         }
     }
 }
